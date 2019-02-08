d0b2a36a16019a710bdd6cb5f1bada74f3f11cdf
Garrett Cannon <gcannon@vectorform.com>
2019-02-08 12:14:24 -0500

    initial commit

--- pkg/README.md
+++
@@ -1,7 +0,0 @@
-## ts-three-stl-loader
-
-enspiral-cherubi's three.js stl-loader, rewritten in TypeScript
-
-## install
-
-`yarn add --save three`
--- pkg/dist-types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-export default class STLLoader {
-    parse(data: any): any;
-    parseBinary(data: any): THREE.BufferGeometry;
-    parseASCII(data: any): any;
-    ensureString(buf: any): string;
-    ensureBinary(buf: any): any;
-}
